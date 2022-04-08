1e4a8a4c0175fa653f9759a3af82abfd4be2a884
UlfetZakirli <ulfetzakirli@gmail.com>
2022-04-08 22:35:12 +0400

    it was created with react

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
@@ -9,6 +9,13 @@ const allcategories=['all',...new Set(items.map((item)=>item.category))];
 function App() {
   const [menuItems,setMenuItems]=useState(items);
   const[categories,setCategories]=useState(allcategories);
+  const [product,setProduct]=useState('');
+  const getProduct=()=>{
+    fetch('./src/data.js')
+    .then(res=>res.json())
+  }
+
+
 
   const filterItems=(category)=>{
     if(category==="all"){
@@ -24,7 +31,9 @@ function App() {
   return <main>
     <section className='menu section'>
       <div className='title'>
-        <h2>our menu</h2>
+        <h2>my Products</h2>
+      <button className='mb-4' onClick={getProduct}>Get Product</button>
+
         <div className='underline'></div>
       </div>
   <Categories categories={categories} filterItems={filterItems}/>
--- setup/src/Menu.js
+++ setup/src/Menu.js
@@ -5,16 +5,34 @@ const Menu = ({items}) => {
     {items.map((menuItem)=>{
       const {id,title,img,desc,price}=menuItem;
       return(
-        <article key={id} className="menu-item">
-        <img src={img} alt={title} className='photo'/>
-        <div className='item-info'> 
-        <header>
-          <h4>{title}</h4>
-          <h4 className='price'>${price}</h4>
-        </header>
-        <p  className='item-text'>{desc}</p>
+        // <article key={id} className="menu-item">
+        // <img src={img} alt={title} className='photo card-img-top'/>
+        // <div className='item-info'> 
+        // <header>
+        //   <h4>{title}</h4>
+        //   <h4 className='price'>${price}</h4>
+        // </header>
+        // <p  className='item-text'>{desc}</p>
+        // <button className="btn btn-dark">
+        //               Buy now
+        //             </button>
+        // </div>
+        // </article>
+        <div className="col-md-6 mb-4">
+        <div className="card">
+          <img src={img} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">${price}</h5>
+            <p className="card-text">
+          {desc}
+            </p>
+            <button className="btn btn-dark">
+              Buy now
+            </button>
+          </div>
         </div>
-        </article>
+      </div>
+
       );
     
     })}
